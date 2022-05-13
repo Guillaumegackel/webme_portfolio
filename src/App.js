f01e570bf9e787e835fd9eab9d73f9f0ad32d871
Webme <guillaume.gackel@gmail.com>
2022-05-13 19:22:14 +0200

    social bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
-      <Navbar/></div>
+      <Navbar/>
+      <Home />
+      
+      </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,82 @@
-import React from "react";
-import { FaBars, faBars, FaTimes } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 import logo from "../assets/WebMelogo.png";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClickMenu = () => setNav(!nav);
+
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
-      <div>
-        <img src={logo} alt="logo" style={{ width: "80px" }} />
+      {/* logo */}
+      <div className="bg-blue-500  p-4">
+        <img  src={logo} alt="logo" style={{ width: "80px" }} />
       </div>
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-      {/* BurgerMenu */}
-      <div className="hidden">
-        <FaBars />
-      </div>
-      {/* MobileMenu */}
-      <ul className="hidden">
+      {/* Menu principal */}
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
         <li>Work</li>
         <li>Contact</li>
       </ul>
+
+      {/* BurgerMenu */}
+      <div onClick={handleClickMenu} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}{" "}
+      </div>
+
+      {/* MobileMenu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
       {/* SocialIcon */}
-      <div></div>
+      <div className=" hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] ease-in duration-300 bg-blue-500">
+            <a 
+			className="flex justify-between items-center w-full text-gray-300" 
+			href="/">
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] ease-in duration-300 bg-[#333333] ">
+            <a 
+			className="flex justify-between items-center w-full text-gray-300" 
+			href="/">
+              GitHub <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] ease-in duration-300 bg-[#6fc2b0]]">
+            <a 
+			className="flex justify-between items-center w-full text-gray-300" 
+			href="/">
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] ease-in duration-300 bg-[#565f69]">
+            <a 
+			className="flex justify-between items-center w-full text-gray-300" 
+			href="/">
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      
+      </div>
     </div>
   );
 };
