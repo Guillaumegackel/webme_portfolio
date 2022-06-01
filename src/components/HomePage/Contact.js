f8076aa9a575007479d0f856cb212c391094d377
Webme <guillaume.gackel@gmail.com>
2022-06-01 16:24:58 +0200

    netlify cli update

--- src/components/HomePage/Contact.js
+++ src/components/HomePage/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-	<div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+	<div name='contact' className='w-full h-full bg-[#0a192f] flex justify-center items-center p-4'>
 		<form method='POST' action="https://getform.io/f/d15e7544-56cd-479a-ac88-34364eec89e0" className='flex flex-col max-w-[600px] w-full'>
 			<div className='pb-8'>
 				<p className='text-4xl font-bold inline border-b-4 border-blue-600 text-gray-300'>Contact</p>
--- src/components/HomePage/Skills.js
+++ src/components/HomePage/Skills.js
@@ -14,7 +14,7 @@ import mui from "../../assets/mui.png";
 
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen pt-10 bg-[#0a192f] text-gray-300">
+    <div name="skills" className="w-full h-full pt-10 bg-[#0a192f] text-gray-300">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
 		  <div className="">
 			  <p className="text-4xl font-bold inline border-b-4 border-blue-600 ">Skills</p>
